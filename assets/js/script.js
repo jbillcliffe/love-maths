4592de0d4da41de5d400f0821db2964565352189
jbillcliffe <>
2023-08-10 22:16:59 +0000

    code to create two numbers for question

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+/**
+ * The main game loop, called when script is first loaded and after the 
+ * user's answer has been processed
+*/
 function runGame() {
-
+    let num1 = Math.floor(Math.random() * 25 + 1);
+    let num2 = Math.floor(Math.random() * 25 + 1);
 }
+
+runGame();
 
 function checkAnswer() {
 
