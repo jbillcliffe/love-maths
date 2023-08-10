2d2773d68f221731ec82a1f22e5318d9d99cf75e
jbillcliffe <>
2023-08-10 22:07:05 +0000

    listeners for document load + btn clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
-console.log("Connected!");
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
 
 function runGame() {
 
